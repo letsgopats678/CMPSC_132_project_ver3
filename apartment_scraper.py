5d86b1b66daf9276fccff9098a33a447991c4142
Eric Phillips <eap5662@psu.edu>
2022-04-23 22:49:25 -0400

    updated webscraper.py again

--- apartment_scraper.py
+++ apartment_scraper.py
@@ -11,6 +11,7 @@ listings = soup.find_all('li', class_="result-row")
 json_file_full_rent = []
 json_file_rent = []
 
+parsed_listings = []
 
 for listing in listings:
     links = []
@@ -50,16 +51,13 @@ for listing in listings:
             return num_prices/num_rooms
 
     json_listing = {
-        "listings": {
         "Posting Date": day_posted.text,
         "Post Name": post_of_flat.text,
         "Number of Bedrooms": bedroom_num,
         "Price Per Month": listing_price.text,
         "Price Per Student": cost_per_person(bedroom_num, price_of_apartment),
-        "Link to Main Post": main_website,
-        "Timestamp Added": datetime.date.today().day,
-        "Test": "success"
-        }
+        "Link": main_website,
+        "Timestamp Added": datetime.date.today().day
     }
 
 
@@ -67,49 +65,8 @@ for listing in listings:
 
 
 
-
-
-
-for listing in listings:
-    links = []
-
-    day_posted = listing.find('time', class_='result-date')
-    listing_price = listing.find('span', class_='result-price')
-    post_of_flat = listing.find('h3', class_='result-heading')
-    main_website = listing.a['href']
-    num_bedrooms = listing.find('span', class_='shared-line-bubble')
-
-    links.append(main_website)
-    for link in links:
-        html_in = requests.get(link)
-        soup = BeautifulSoup(html_in.text, 'lxml')
-
-        linkers = soup.find('p', class_="attrgroup")
-        rooms = linkers.find('span', class_='shared-line-bubble')
-
-        bedrooms = rooms.text
-        bedroom_num = float(bedrooms[0:1])
-
-
-
-
-
-
-    json_listing2 = {
-        "listings": {
-        "Posting Date": day_posted.text,
-        "Post Name": post_of_flat.text,
-        "Number of Bedrooms": bedroom_num,
-        "Price Per Month": price_of_apartment,
-        "Link to Main Post": main_website,
-        "Time Added": datetime.date.today().day
-            }
-        }
-    json_file_rent.append(json_listing2)
-
 # Serializing json
 json_object = json.dumps(json_file_full_rent, indent=4)
-json_object2 = json.dumps(json_file_rent, indent=3)
 
 with open("listings_roommates.json", "r") as readfile:
     raw_data = readfile.read()
