66233555494c659b9aeb7d29b30ecd77df67d2d1
Takiur Rahman <takiur123@gmail.com>
2022-04-25 00:10:49 -0400

    Main

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
-
+from bs4 import BeautifulSoup
+import requests
+import json
+import datetime
+from apartment_scraper import get_listings
 app = Flask(__name__)
 
 
@@ -12,9 +16,11 @@ def calculate():
     meal = 0
     misnsavings = 0
     household_size = 0
+    total_rent= 0
+    parsed_list = []
     if request.method == 'POST' and 'income' in request.form and 'Household_size' in request.form:
         income = float(request.form.get('income'))
-        min_rent = 300
+
 
         #Transportation
         priv_tran = float(request.form.get('tran_cost'))
@@ -25,7 +31,7 @@ def calculate():
 
         #meal
         if request.form.get('yes_mealp'):
-            meal = 167 +  280
+            meal = 167 + 280
         else:
             meal = 296
 
@@ -34,9 +40,20 @@ def calculate():
         utilities = (((9 + household_size)/10)*200)/household_size
 
         #miscellenious and savings
-        misnsavings = income * 0.15
-        rent =(income - transportation - meal - utilities - misnsavings)
-    return render_template('Home.html', rent=rent, transportation=transportation, meal=meal, utilities=utilities, misnsavings=misnsavings, household_size=household_size)
+        if income <= 700:
+            misnsavings = income * .05
+        elif (income > 700) and (income <= 1000):
+            misnsavings = income * .08
+        else:
+            misnsavings = income * 0.12
+
+
+        rent = (income - transportation - meal - utilities - misnsavings)
+
+        total_rent = household_size * rent
+    get_listings(rent, household_size, parsed_list)
+
+    return render_template('Home.html', rent=rent, transportation=transportation, meal=meal, utilities=utilities, misnsavings=misnsavings, household_size=household_size, parsed_list=parsed_list, len=len(parsed_list))
 
 
 if __name__ == '__main__':
